6ef0f4751ea4fcbffa150fc738605558ab704214
JungWooJin97 <wjin9707@naver.com>
2021-05-12 15:34:50 +0900

    CSS_2+Python1

---
+++ Python1/first.py
@@ -0,0 +1,11 @@
+print(1+2)
+
+banzang="정우진"
+print("우리반 반장 이름은", banzang)
+
+banjang=input("반장 이름을 입력하세요.")
+print("우리반 반장 이름은", banjang)
+
+number1 = int(input("첫번쨰 숫자"))
+number2 = int(input("두번째 숫자"))
+print(number1+number2)
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
   웹 기초 수강- introduce_myself_page  
   HTML 수강- HTML_basic  
   CSS1 수강- CSS_1  
+  CSS2, Python1 수강- CSS_2, Python1
